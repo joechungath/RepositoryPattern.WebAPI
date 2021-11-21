48be0748aa92165acad207cd5ae1fb96fd3e22e6
JChungath <jchungath@github.com>
2021-11-21 00:08:46 -0600

    Add project files.

---
+++ Domain/Developer.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Joe";
+        public int Followers { get; set; } = 5;
+    }
+}
